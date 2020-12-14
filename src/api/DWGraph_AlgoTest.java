7a4417e046d257e478c3f7d9408496831d200c16
bar schtalman <barschtalman@gmail.com>
2020-12-14 16:28:48 +0200

    tests added

--- src/api/DWGraph_AlgoTest.java
+++ src/api/DWGraph_AlgoTest.java
@@ -2,13 +2,23 @@ package api;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DWGraph_AlgoTest {
     @Test
     void init(){
-
+        directed_weighted_graph g = new DWGraph_DS();
+        dw_graph_algorithms ga = new DWGraph_Algo();
+        ga.init(g);
+        assertEquals(g, new DWGraph_DS());//not working
+        g.addNode(new Node(2));
+        assertNotEquals(g, new DWGraph_DS());
+        assertEquals(g, ga.getGraph());
     }
     @Test
     void copy(){
+        directed_weighted_graph g = new DWGraph_DS();
+        dw_graph_algorithms ga = new DWGraph_Algo();
 
     }
     @Test
@@ -21,6 +31,6 @@ public class DWGraph_AlgoTest {
     }
     @Test
     void shortestPath(){
-        
+
     }
 }
